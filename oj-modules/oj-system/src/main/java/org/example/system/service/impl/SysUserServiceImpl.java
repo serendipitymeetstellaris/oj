ab9c94c6bd77c166b25055d8e6127dd6e84ef8f6
serendipitymeetstellaris <serendipitymeetstellaris@gmail.com>
2026-04-08 23:57:07 +0800

    注释以及导入语句修改

--- oj-modules/oj-system/src/main/java/org/example/system/service/impl/SysUserServiceImpl.java
+++ oj-modules/oj-system/src/main/java/org/example/system/service/impl/SysUserServiceImpl.java
@@ -3,7 +3,6 @@ package org.example.system.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import org.example.common.core.domain.R;
 import org.example.common.core.enums.ResultCode;
-import org.example.system.controller.LoginResult;
 import org.example.system.domain.SysUser;
 import org.example.system.mapper.SysUserMapper;
 import org.example.system.service.ISysUserService;
@@ -18,7 +17,6 @@ public class SysUserServiceImpl implements ISysUserService {
     private SysUserMapper sysUserMapper;
 
     @Override
-    //维护性、性能、安全
     public R<Void> login(String userAccount, String password) {
         //通过账号去数据库中查询，对应的用户信息
         LambdaQueryWrapper<SysUser> queryWrapper = new LambdaQueryWrapper<>();
